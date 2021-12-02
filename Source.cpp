41a3e55df3f54e12ee7770e19fb0e4cdb052debf
Ryankhoury <92649927+Ryankhoury@users.noreply.github.com>
2021-12-02 16:32:37 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,52 +6,80 @@
 
 using namespace std;
 
-void main(int argc, char* argv[], string start, string end, int steps) {
-	//5 counters for the five vowels to detect number of vowels in the string readed
-	int a=0, e=0,i=0,o=0,u=0;
-	
-	cout << setfill('*')<<setw(60)<<""<<endl;
-	cout << setfill('*')<<setw(12)<<""<<" welcome to my letter count program "<< setfill('*')<<  setw(12) <<""<<endl;
-	cout << setfill('*') << setw(60) << "" << endl << endl;
-	// Create a text string, which is used to output the text file
-	string myText;
+int main()
+{
+    //5 counters for the five vowels to detect number of vowels in the string readed
+    int a=0, e=0,i=0,o=0,u=0,l=0,m=0,g=0;
 
-	// Read from the text file
-	ifstream MyReadFile(argv[1]);
+    cout << setfill('*')<<setw(60)<<""<<endl;
+    cout << setfill('*')<<setw(12)<<""<<" Welcome to my letter count program "<< setfill('*')<<  setw(12) <<""<<endl;
+    cout << setfill('*') << setw(60) << "" << endl << endl;
 
-	// Use a while loop together with the getline() function to read the file line by line
-	cout << "Analayzing file '" << argv[1]<<"'"<< setfill('.')<<setw(3)<<""<<endl<<endl;
-	while (getline(MyReadFile, myText)) {
-		// Output the text from the file
-		// tolowwer function to make the letter in lowwer case
-		//string.at(i) gets the character at index i in the string
-		for (int i = 0; i < myText.length(); i++) {
-			if (tolower(myText.at(i))=='a') {
-				a++;//every a we incremnent value of a's counter by 1
-			}
-			else if (tolower(myText.at(i)) == 'e') {
-				e++;//every e we incremnent value of e's counter by 1
-			}
-			else if (tolower(myText.at(i)) == 'i') {
-				i++;//every i we incremnent value of i's counter by 1
-			}
-			else if (tolower(myText.at(i)) == 'o') {
-				o++;//every o we incremnent value of o's counter by 1
-			}
-			else if (tolower(myText.at(i)) == 'u') {
-				u++;//every u we incremnent value of u's counter by 1
-			}
-		}
-		
-	}
-	//print the statements of each vowels with its number of occurence
-	cout << "The number of A's: " << setfill('.') <<setw(40) <<"" <<a<< endl;
-	cout << "The number of E's: " << setfill('.') << setw(40) << "" << e << endl;
-	cout << "The number of I's: " << setfill('.') << setw(40) << "" << i << endl;
-	cout << "The number of O's: " << setfill('.') << setw(40) << "" << o << endl;
-	cout << "The number of U's: " << setfill('.') << setw(40) << "" << u << endl;
-	cout << "The vowel count is: " << setfill('.') << setw(39) << "" << a+e+i+o+u << endl;
+    // Read from the text file
+    ifstream MyReadFile;
 
-	// Close the file
-	MyReadFile.close();
+    //Open the file
+    MyReadFile.open("Constitution_of_the_United_States.html" , ios :: in ) ;
+
+    cout << "Analyzing file '"<<setfill('.')<<setw(3)<<"'"<<endl<<endl;
+
+    //Create char to go through the file
+    char ch ;
+
+    //While loop to go through the file until eof()
+    while (!(MyReadFile.eof()))
+    {
+        MyReadFile >> ch ;
+        // Output the text from the file
+        // tolower function to make the letter in lowwer case
+
+            if (tolower(ch)=='a')
+            {
+                a++;//every a we incremnent value of a's counter by 1
+            }
+            else if (tolower(ch) == 'e')
+            {
+                e++;//every e we incremnent value of e's counter by 1
+            }
+            else if (tolower(ch) == 'i')
+            {
+                i++;//every i we incremnent value of i's counter by 1
+            }
+            else if (tolower(ch) == 'o')
+            {
+                o++;//every o we incremnent value of o's counter by 1
+            }
+            else if (tolower(ch) == 'u')
+            {
+                u++;//every u we incremnent value of u's counter by 1
+            }
+            else if (tolower(ch) == 'm')
+            {
+                m++;//every u we incremnent value of m's counter by 1
+            }
+            else if (tolower(ch) == 'g')
+            {
+                g++;//every u we incremnent value of g's counter by 1
+            }
+            else if (tolower(ch) == 'l')
+            {
+                l++;//every u we incremnent value of l's counter by 1
+            }
+
+    }
+    //print the statements of each vowels with its number of occurence
+    cout << "The number of A's: " << setfill('.') <<setw(40) <<"" <<a<< endl;
+    cout << "The number of E's: " << setfill('.') << setw(40) << "" << e << endl;
+    cout << "The number of I's: " << setfill('.') << setw(40) << "" << i << endl;
+    cout << "The number of O's: " << setfill('.') << setw(40) << "" << o << endl;
+    cout << "The number of U's: " << setfill('.') << setw(40) << "" << u << endl;
+    cout << "The vowel count is: " << setfill('.') << setw(39) << "" << a+e+i+o+u << endl;
+    cout << "The number of L's: " << setfill('.') << setw(40) << "" << l << endl;
+    cout << "The number of M's: " << setfill('.') << setw(40) << "" << m << endl;
+    cout << "The number of G's: " << setfill('.') << setw(40) << "" << g << endl;
+
+    // Close the file
+    MyReadFile.close();
+
+    return 0 ;
 }
